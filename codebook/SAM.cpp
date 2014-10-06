f6580383368a361bf9653880b11c7c75a4d6b9cc
c2251393 <c2251393@gmail.com>
2014-10-06 21:25:37 +0800

    add Suffix Automaton

---
+++ codebook/SAM.cpp
@@ -0,0 +1,68 @@
+
+const int MAXM = 1000010;
+
+struct SAM
+{
+  int tot, root, lst, mom[MAXM], mx[MAXM];
+  int acc[MAXM], nxt[MAXM][33];
+  int newNode()
+  {
+    int res = ++tot;
+    fill(nxt[res], nxt[res]+33, 0);
+    mom[res] = mx[res] = acc[res] = 0;
+    return res;
+  }
+  void init()
+  {
+    tot = 0;
+    root = newNode();
+    mom[root] = 0, mx[root] = 0;
+    lst = root;
+  }
+  void push(int c)
+  {
+    int p = lst;
+    int np = newNode();
+    mx[np] = mx[p]+1;
+    for(; p && nxt[p][c] == 0; p = mom[p])
+      nxt[p][c] = np;
+    if(p == 0) mom[np] = root;
+    else
+    {
+      int q = nxt[p][c];
+      if(mx[p]+1 == mx[q]) mom[np] = q;
+      else
+      {
+        int nq = newNode();
+        mx[nq] = mx[p]+1;
+        for(int i = 0; i < 33; i++)
+          nxt[nq][i] = nxt[q][i];
+        mom[nq] = mom[q];
+        mom[q] = nq;
+        mom[np] = nq;
+        for(; p && nxt[p][c] == q; p = mom[p])
+          nxt[p][c] = nq;
+      }
+    }
+    lst = np;
+  }
+  void print()
+  {
+    REP(i, 1, tot)
+    {
+      printf("node %d :\n", i);
+      printf("mx %d, mom %d\n", mx[i], mom[i]);
+      REP(j, 1, 26) if(nxt[i][j])
+        printf("nxt %c %d\n", 'a'+j-1, nxt[i][j]);
+      puts("---------------------------");
+    }
+  }
+  void push(char *str)
+  {
+    for(int i = 0; str[i]; i++)
+      push(str[i]-'a'+1);
+  }
+};
+
+SAM sam;
+
