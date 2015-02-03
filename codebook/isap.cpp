7fb6496a73d25f622af9abc341d34e97ff683515
akaiNeko <c2251393@gmail.com>
2015-02-03 11:20:29 +0800

    add isap flow with good apis

---
+++ codebook/isap.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define SZ(c) ((int)(c).size())
+using namespace std;
+struct Maxflow {
+  static const int MAXV = 20010;
+  static const int INF  = 1000000;
+  struct Edge {
+    int v, c, r;
+    Edge(int _v, int _c, int _r) : v(_v), c(_c), r(_r) {}
+  };
+  int s, t;
+  vector<Edge> G[MAXV*2];
+  int iter[MAXV*2], d[MAXV*2], gap[MAXV*2], tot;
+  void flowinit(int x) {
+    tot = x+2;
+    s = x+1, t = x+2;
+    for(int i = 0; i <= tot; i++) {
+      G[i].clear();
+      iter[i] = d[i] = gap[i] = 0;
+    }
+  }
+  void addEdge(int u, int v, int c) {
+    G[u].push_back(Edge(v, c, SZ(G[v]) ));
+    G[v].push_back(Edge(u, 0, SZ(G[u]) - 1));
+  }
+  int dfs(int p, int flow) {
+    if(p == t) return flow;
+    for(int &i = iter[p]; i < SZ(G[p]); i++) {
+      Edge &e = G[p][i];
+      if(e.c > 0 && d[p] == d[e.v]+1) {
+        int f = dfs(e.v, min(flow, e.c));
+        if(f) {
+          e.c -= f;
+          G[e.v][e.r].c += f;
+          return f;
+        }
+      }
+    }
+    if( (--gap[d[p]]) == 0) d[s] = tot;
+    else {
+      d[p]++;
+      iter[p] = 0;
+      ++gap[d[p]];
+    }
+    return 0;
+  }
+
+  int maxflow() {
+    //puts("MF");
+    int res = 0;
+    gap[0] = tot;
+    for(res = 0; d[s] < tot; res += dfs(s, INF));
+    return res;
+  }
+} flow;
+
+Maxflow::Edge e(1, 1, 1);
+
