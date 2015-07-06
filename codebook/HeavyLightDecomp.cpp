6b4a828a63fa1a29ac02b728c3500cd4cb64cba8
akaiNeko <c2251393@gmail.com>
2015-07-06 13:47:45 +0800

    add HLD template

---
+++ codebook/HeavyLightDecomp.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SZ(c) (int)(c).size()
+#define ALL(c) (c).begin(), (c).end()
+#define REP(i, s, e) for(int i = (s); i <= (e); i++)
+#define REPD(i, s, e) for(int i = (s); i >= (e); i--)
+
+const int MAXN = 100010;
+const int LOG  = 19;
+
+struct HLD
+{
+  int n;
+  vector<int> g[MAXN];
+  int sz[MAXN], dep[MAXN];
+  int ts, tid[MAXN], tdi[MAXN], tl[MAXN], tr[MAXN];
+  int mom[MAXN][LOG], head[MAXN];
+  void dfssz(int u, int p)
+  {
+    dep[u] = dep[p] + 1;
+    mom[u][0] = p;
+    sz[u] = 1;
+    head[u] = u;
+    for(int& v:g[u]) if(v != p)
+    {
+      dep[v] = dep[u] + 1;
+      dfssz(v, u);
+      sz[u] += sz[v];
+    }
+  }
+  void dfshl(int u)
+  {
+    //printf("dfshl %d\n", u);
+    ts++;
+    tid[u] = tl[u] = tr[u] = ts;
+    tdi[tid[u]] = u;
+    sort(ALL(g[u]), [&](int a, int b){return sz[a] > sz[b];});
+    bool flag = 1;
+    for(int& v:g[u]) if(v != mom[u][0])
+    {
+      if(flag) head[v] = head[u], flag = 0;
+      dfshl(v);
+      tr[u] = tr[v];
+    }
+  }
+  inline int lca(int a, int b)
+  {
+    if(dep[a] > dep[b]) swap(a, b);
+    //printf("lca %d %d\n", a, b);
+    int diff = dep[b] - dep[a];
+    REPD(k, LOG-1, 0) if(diff & (1<<k))
+    {
+      //printf("b %d\n", mom[b][k]);
+      b = mom[b][k];
+    }
+    if(a == b) return a;
+    REPD(k, LOG-1, 0) if(mom[a][k] != mom[b][k])
+    {
+      a = mom[a][k];
+      b = mom[b][k];
+    }
+    return mom[a][0];
+  }
+  void init( int _n )
+  {
+    n = _n;
+    REP( i , 1 , n ) g[ i ].clear();
+  }
+  void addEdge( int u , int v )
+  {
+    g[ u ].push_back( v );
+    g[ v ].push_back( u );
+  }
+  void yutruli()
+  {
+    dfssz(1, 0);
+    ts = 0;
+    dfshl(1);
+    REP(k, 1, LOG-1) REP(i, 1, n)
+      mom[i][k] = mom[mom[i][k-1]][k-1];
+  }
+  vector< tii > getPath( int u , int v )
+  {
+    vector< tii > res;
+    while( tid[ u ] < tid[ head[ v ] ] )
+    {
+      res.push_back( tii( tid[ head[ v ] ] , tid[ v ] ) );
+      v = mom[ head[ v ] ][ 0 ];
+    }
+    res.push_back( tii( tid[ u ] , tid[ v ] ) );
+    reverse( ALL( res ) );
+    return res;
+  }
+} tree;
