5b8ffdcc4f92cdc258848b4550b7285970f46bf8
eddy1021 <c.c.hsu01@gmail.com>
2015-10-17 21:24:41 +0800

    add k.cpp

--- contest/NCPC2015/k.cpp
+++ contest/NCPC2015/k.cpp
@@ -1,55 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-int __ = 1 , n , s , k;
-vector<int> a;
-int sumb , sumc , suma;
+#define N 9
+int __, n = 6;
+int a[ N ] , b[ N ] , c[ N ] , bs[ N ];
+set<int> s1 , s2;
+typedef set<int>::iterator si;
 void init(){
-  scanf( "%d%d%d" , &n , &s , &k );
-  a.clear();
-  sumb = sumc = 0;
-  for( int i = 0 ; i < n ; i ++ ){
-    int _a, _b, _c;
-    scanf( "%d%d%d" , &_a , &_b , &_c );
-    a.push_back( _a );
-    sumb += _b;
-    sumc += _c;
-  }
-  suma = 0;
-  sort( a.begin() , a.end() );
-  a.resize( k );
-  for( int i = 0 ; i < k ; i ++ )
-    suma += a[ i ];
+  for( int i = 0 ; i < n ; i ++ )
+    scanf( "%d" , &a[ i ] );
+  for( int i = 0 ; i < n ; i ++ )
+    scanf( "%d" , &b[ i ] );
+  for( int i = 0 ; i < n ; i ++ )
+    c[ i ] = i;
 }
-#define N 15010
-bool dp[ N * 100 ];
 void solve(){
-  if( a.back() * 2 > s ){
-    puts( "NO" );
-    return;
-  }
-  if( sumb >= suma || sumc >= suma ){
-    puts( "YES" );
-    return;
-  }
-  if( sumb > sumc ) swap( sumb , sumc );
-  for( int i = 0 ; i <= sumb ; i ++ ) dp[ i ] = false;
-  dp[ 0 ] = true;
-  int nmax = 0;
-  for( int i = 0 ; i < k ; i ++ )
-    for( int j = min( sumb , nmax + a[ i ] )
-        ; j >= a[ i ] ; j -- )
-      if( dp[ j - a[ i ] ] ){
-        dp[ j ] = true;
-        nmax = max( nmax , j );
-      }
-  for( int i = 0 ; i <= sumb ; i ++ )
-    if( dp[ i ] && suma - i <= sumc ){
-      puts( "YES" );
-      return;
+  s1.clear();
+  s2.clear();
+  do{
+    int num1 = 0, num2 = 0;
+    for( int i = 0 ; i < n ; i ++ ){
+      num1 = num1 + bs[ i ] * a[ c[ i ] ];
+      num2 = num2 + bs[ i ] * b[ c[ i ] ];
+      s1.insert( num1 );
+      s2.insert( num2 );
     }
-  puts( "NO" );
+  }while( next_permutation( c , c + n ) );
+  int ans1 = 0, ans2 = 0;
+  for( si it = s1.begin() ; it != s1.end() ; it ++ )
+    if( s2.count( *it ) == 0 ) ans1 ++;
+  for( si it = s2.begin() ; it != s2.end() ; it ++ )
+    if( s1.count( *it ) == 0 ) ans2 ++;
+  printf( "%d %d\n" , ans1 , ans2 );
 }
 int main(){
+  bs[ 0 ] = 1;
+  for( int i = 1 ; i < N ; i ++ )
+    bs[ i ] = bs[ i - 1 ] * 10;
   scanf( "%d" , &__ ); while( __ -- ){
     init();
     solve();
