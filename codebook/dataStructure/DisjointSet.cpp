2a0421d28f90992fc679ceda8b188436aceee233
eddy1021 <c.c.hsu01@gmail.com>
2016-01-07 22:21:39 +0800

    fix ds/dsu ce problem

--- codebook/dataStructure/DisjointSet.cpp
+++ codebook/dataStructure/DisjointSet.cpp
@@ -1,39 +1,39 @@
-struct DisjointSet {
+struct DisjointSet{
   // save() is like recursive
   // undo() is like return
-  int n, fa[MXN], sz[MXN];
-  vector<pair<int*,int>> h;
+  int n, fa[ N ], sz[ N ];
+  vector< pair<int*,int> > h;
   vector<int> sp;
-  void init(int tn) {
+  void init( int tn ){
     n=tn;
-    for (int i=0; i<n; i++) {
-      fa[i]=i;
-      sz[i]=1;
+    for( int i = 0 ; i < n ; i ++ ){
+      fa[ i ]=i;
+      sz[ i ]=1;
     }
     sp.clear(); h.clear();
   }
-  void assign(int *k, int v) {
-    h.PB({k, *k});
-    *k=v;
+  void assign( int *k, int v ){
+    h.PB( {k, *k} );
+    *k = v;
   }
-  void save() { sp.PB(SZ(h)) };
-  void undo() {
+  void save(){ sp.PB(SZ(h)); }
+  void undo(){
     assert(!sp.empty());
     int last=sp.back(); sp.pop_back();
-    while (SZ(h)!=last) {
+    while( SZ(h)!=last ){
       auto x=h.back(); h.pop_back();
-      *x.F=x.S;
+      *x.first = x.second;
     }
   }
-  int f(int x) {
-    while (fa[x]!=x) x=fa[x];
+  int f( int x ){
+    while( fa[ x ] != x ) x = fa[ x ];
     return x;
   }
-  void uni(int x, int y) {
-    x=f(x); y=f(y);
-    if (x==y) return ;
-    if (sz[x]<sz[y]) swap(x, y);
-    assign(&sz[x], sz[x]+sz[y]);
-    assign(&fa[y], x);
+  void uni( int x , int y ){
+    x = f( x ); y = f( y );
+    if( x == y ) return;
+    if( sz[ x ] < sz[ y ] ) swap( x, y );
+    assign( &sz[ x ] , sz[ x ] + sz[ y ] );
+    assign( &fa[ y ] , x);
   }
 }djs;
