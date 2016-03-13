7f2f3df7ed50c0ef1651cbd5e890be7200a3f7ec
eddy1021 <c.c.hsu01@gmail.com>
2016-03-14 00:29:06 +0800

    add comment for intersection of 2 circles

--- codebook/geometry/Intersection_of_two_circles.cpp
+++ codebook/geometry/Intersection_of_two_circles.cpp
@@ -1,9 +1,9 @@
-vector<pdd> interCircle(pdd o1, double r1, pdd o2, double r2) {
-  ld d2 = (o1 - o2) * (o1 - o2);
-  ld d = sqrt(d2);
+vector<pdd> interCircle( pdd o1, D r1 , pdd o2 , D r2 ){
+  D d2 = (o1 - o2) % (o1 - o2); // dot
+  D d = sqrt(d2);
 	if (d > r1+r2) return {};
   pdd u = 0.5*(o1+o2) + ((r2*r2-r1*r1)/(2*d2))*(o1-o2);
-  double A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
-  pdd v = A / (2*d2) * pdd(o1.S-o2.S, -o1.F+o2.F);
+  D A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
+  pdd v = A / (2*d2) * pdd(o1.Y-o2.Y, -o1.X+o2.X);
   return {u+v, u-v};
 }
