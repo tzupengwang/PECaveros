5b7f45c3f0823042cb26e6cddcfc97d192c58675
eddy1021 <c.c.hsu01@gmail.com>
2016-04-10 18:43:50 +0800

    update miller rabin

--- codebook/math/Miller_Rabin.cpp
+++ codebook/math/Miller_Rabin.cpp
@@ -3,8 +3,10 @@
 // n < 3,474,749,660,383          6 :  pirmes <= 13
 // n < 2^64                       7 :
 // 2, 325, 9375, 28178, 450775, 9780504, 1795265022
-ll power(ll x,ll p,ll mod){
-	ll s=1,m=x;
+// Make sure testing integer is in range [2, n−2] if
+// you want to use magic.
+long long power(long long x,long long p,long long mod){
+	long long s=1,m=x;
 	while(p) {
 		if(p&1) s=mult(s,m,mod);
 		p>>=1;
@@ -12,29 +14,29 @@ ll power(ll x,ll p,ll mod){
 	}
 	return s;
 }
-bool witness(ll a,ll n,ll u,int t){
-	ll x=power(a,u,n);
+bool witness(long long a,long long n,long long u,int t){
+	long long x=power(a,u,n);
 	for(int i=0;i<t;i++) {
-		ll nx=mult(x,x,n);
+		long long nx=mult(x,x,n);
 		if(nx==1&&x!=1&&x!=n-1) return 1;
 		x=nx;
 	}
 	return x!=1;
 }
-bool miller_rabin(ll n,int s=100) {
+bool miller_rabin(long long n,int s=100) {
 	// iterate s times of witness on n
 	// return 1 if prime, 0 otherwise
 	if(n<2) return 0;
 	if(!(n&1)) return n==2;
-	ll u=n-1;
+	long long u=n-1;
 	int t=0;
 	// n-1 = u*2^t
-	while(u&1) {
+	while(!(u&1)) {
 		u>>=1;
 		t++;
 	}
 	while(s--) {
-		ll a=randll()%(n-1)+1;
+		long long a=randll()%(n-1)+1;
 		if(witness(a,n,u,t)) return 0;
 	}
 	return 1;
