4f17142e939e07d7679e7a708f7e82711722cf7c
eddy1021 <c.c.hsu01@gmail.com>
2016-07-02 21:34:05 +0800

    update codebook

--- codebook/geometry/Intersection_of_two_circles.cpp
+++ codebook/geometry/Intersection_of_two_circles.cpp
@@ -2,8 +2,8 @@ vector<Pt> interCircle( Pt o1 , D r1 , Pt o2 , D r2 ){
   D d2 = ( o1 - o2 ) * ( o1 - o2 );
   D d = sqrt(d2);
 	if( d > r1 + r2 ) return {};
-  Pt u = ( o1 + o2 ) * 0.5 + ( o1 - o2 ) * ((r2*r2-r1*r1)/(2*d2));
-  D  A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
+  Pt u = (o1+o2)*0.5 + (o1-o2)*((r2*r2-r1*r1)/(2*d2));
+  D A = sqrt((r1+r2+d)*(r1-r2+d)*(r1+r2-d)*(-r1+r2+d));
   Pt v = Pt( o1.Y-o2.Y , -o1.X + o2.X ) * A / (2*d2);
   return {u+v, u-v};
 }
--- codebook/geometry/Intersection_of_two_segments.cpp
+++ codebook/geometry/Intersection_of_two_segments.cpp
@@ -12,6 +12,6 @@ bool banana( const PLL& p1 , const PLL& p2 ,
            ( ( q1 - p1 ) * ( q2 - p1 ) ) <= 0 ||
            ( ( q1 - p2 ) * ( q2 - p2 ) ) <= 0;
   }
-  return ( ori( p1 , p2 , q1 ) * ori( p1 , p2 , q2 ) <= 0 ) &&
-         ( ori( q1 , q2 , p1 ) * ori( q1 , q2 , p2 ) <= 0 );
+  return (ori( p1, p2, q1 ) * ori( p1, p2, q2 )<=0) &&
+         (ori( q1, q2, p1 ) * ori( q1, q2, p2 )<=0);
 }
