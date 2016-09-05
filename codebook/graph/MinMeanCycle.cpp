6d86f79760e9be0610afd43ac2575c1e295d00a0
eddy1021 <c.c.hsu01@gmail.com>
2016-09-05 11:35:57 +0800

    add MinMeanCycle

---
+++ codebook/graph/MinMeanCycle.cpp
@@ -0,0 +1,63 @@
+/* minimum mean cycle O(VE) */
+struct MMC{
+#define MAXE 101010
+#define MAXN 1021
+#define inf 1e9
+#define eps 1e-6
+  struct Edge {
+    int v,u;
+    double c;
+  };
+  int n, m, prv[MAXN][MAXN], prve[MAXN][MAXN], vst[MAXN];
+  Edge e[MAXE];
+  vector<int> edgeID, cycle, rho;
+  double d[MAXN][MAXN];
+  void init( int _n ){
+    n = _n; m = 0;
+  }
+  void add_edge( int vi , int ui , int ci ){
+    e[ m ++ ] = { vi , ui , (double)ci }; 
+  }
+  void bellman_ford() {
+    for(int i=0; i<n; i++) d[0][i]=0;
+    for(int i=0; i<n; i++) {
+      fill(d[i+1], d[i+1]+n, inf);
+      for(int j=0; j<m; j++) {
+        int v = e[j].v, u = e[j].u;
+        if(d[i][v]<inf && d[i+1][u]>d[i][v]+e[j].c) {
+          d[i+1][u] = d[i][v]+e[j].c;
+          prv[i+1][u] = v;
+          prve[i+1][u] = j;
+        }
+      }
+    }
+  }
+  double solve(){
+    // returns inf if no cycle, mmc otherwise
+    double mmc=inf;
+    int st = -1;
+    bellman_ford();
+    for(int i=0; i<n; i++) {
+      double avg=-inf;
+      for(int k=0; k<n; k++) {
+        if(d[n][i]<inf-eps)	avg=max(avg,(d[n][i]-d[k][i])/(n-k));
+        else avg=max(avg,inf);
+      }
+      if (avg < mmc) tie(mmc, st) = tie(avg, i);
+    }
+    FZ(vst); edgeID.clear(); cycle.clear(); rho.clear();
+    for (int i=n; !vst[st]; st=prv[i--][st]) {
+      vst[st]++;
+      edgeID.PB(prve[i][st]);
+      rho.PB(st);
+    }
+    while (vst[st] != 2) {
+      int v = rho.back(); rho.pop_back();
+      cycle.PB(v);
+      vst[v]++;
+    }
+    reverse(ALL(edgeID));
+    edgeID.resize(SZ(cycle));
+    return mmc;
+  }
+} mmc;
