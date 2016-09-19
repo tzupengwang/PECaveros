b87afd7fb599f7cb107a8b96ec818fa195dab61d
eddy1021 <c.c.hsu01@gmail.com>
2016-09-19 22:56:17 +0800

    add bounded max flow and update flow method

---
+++ codebook/flow/BoundedMaxflow.cpp
@@ -0,0 +1,45 @@
+// Max flow with lower/upper bound on edges
+// source = 1 , sink = n
+int in[ N ] , out[ N ];
+int l[ M ] , r[ M ] , a[ M ] , b[ M ];
+int solve(){
+  flow.init( n );
+  for( int i = 0 ; i < m ; i ++ ){
+    in[ r[ i ] ] += a[ i ];
+    out[ l[ i ] ] += a[ i ];
+    flow.addEdge( l[ i ] , r[ i ] , b[ i ] - a[ i ] );
+    // flow on edge from l[ i ] to r[ i ] should
+    // be in [a[ i ], b[ i ]].
+  }
+  int nd = 0;
+  for( int i = 1 ; i <= n ; i ++ ){
+    if( in[ i ] < out[ i ] ){
+      flow.addEdge( i , flow.t , out[ i ] - in[ i ] );
+      nd += out[ i ] - in[ i ];
+    }
+    if( out[ i ] < in[ i ] )
+      flow.addEdge( flow.s , i , in[ i ] - out[ i ] );
+  }
+  // original sink to source
+  flow.addEdge( n , 1 , INF );
+  if( flow.maxflow() != nd )
+    // no solution
+    return -1;
+  int ans = flow.G[ 1 ].back().c; // source to sink
+  flow.G[ 1 ].back().c = flow.G[ n ].back().c = 0;
+  // take out super source and super sink
+  for( size_t i = 0 ; i < flow.G[ flow.s ].size() ; i ++ ){
+    flow.G[ flow.s ][ i ].c = 0;
+    Edge &e = flow.G[ flow.s ][ i ];
+    flow.G[ e.v ][ e.r ].c = 0;
+  }
+  for( size_t i = 0 ; i < flow.G[ flow.t ].size() ; i ++ ){
+    flow.G[ flow.t ][ i ].c = 0;
+    Edge &e = flow.G[ flow.t ][ i ];
+    flow.G[ e.v ][ e.r ].c = 0;
+  }
+  flow.addEdge( flow.s , 1 , INF );
+  flow.addEdge( n , flow.t , INF );
+  flow.reset();
+  return ans + flow.maxflow();
+}
