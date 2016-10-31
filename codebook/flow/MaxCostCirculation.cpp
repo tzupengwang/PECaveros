095615e787cb4bc9a84f32fef04d40871eb589ba
c2251393 <b03902083@ntu.edu.tw>
2016-10-31 16:58:36 +0800

    Create MaxCostCirculation.cpp

---
+++ codebook/flow/MaxCostCirculation.cpp
@@ -0,0 +1,73 @@
+
+struct MaxCostCirc {
+
+  static const int MAXN = 33;
+  int n , m;
+  struct Edge {
+    int v , w , c , r;
+  };
+  vector<Edge> g[ MAXN ];
+  int dis[ MAXN ] , prv[ MAXN ] , prve[ MAXN ];
+  bool vis[ MAXN ];
+  int ans;
+
+  void init( int _n , int _m ) {
+    n = _n , m = _m;
+  }
+
+  inline void adde( int u , int v , int w , int c ) {
+    g[ u ].push_back( { v , w , c , SZ( g[ v ] ) } );
+    g[ v ].push_back( { u , -w , 0 , SZ( g[ u ] )-1 } );
+  }
+
+  inline bool poscyc() {
+    fill( dis , dis+n+1 , 0 );
+    fill( prv , prv+n+1 , 0 );
+    fill( vis , vis+n+1 , 0 );
+    int tmp = -1;
+    FOR( t , n+1 ) {
+      REP( i , 1 , n ) {
+        FOR( j , SZ( g[ i ] ) ) {
+          Edge& e = g[ i ][ j ];
+          if( e.c && dis[ e.v ] < dis[ i ]+e.w ) {
+            dis[ e.v ] = dis[ i ]+e.w;
+            prv[ e.v ] = i;
+            prve[ e.v ] = j;
+            if( t == n ) {
+              tmp = i;
+              break;
+            }
+          }
+        }
+      }
+    }
+    if( tmp == -1 ) return 0;
+    int cur = tmp;
+    while( !vis[ cur ] ) {
+      vis[ cur ] = 1;
+      cur = prv[ cur ];
+    }
+    int now = cur;
+    int cost = 0 , df = 100000;
+
+    do {
+      Edge &e = g[ prv[ now ] ][ prve[ now ] ];
+      df = min( df , e.c );
+      cost += e.w;
+      now = prv[ now ];
+    } while( now != cur );
+    ans += df*cost;
+
+    now = cur;
+    do {
+      Edge &e = g[ prv[ now ] ][ prve[ now ] ];
+      Edge &re = g[ now ][ e.r ];
+      e.c -= df;
+      re.c += df;
+      now = prv[ now ];
+    } while( now != cur );
+
+    return 1;
+  }
+
+} circ;
