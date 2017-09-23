bbae3ec429332b0af39d2271a9a1bfc4ead53c84
eddy1021 <c.c.hsu01@gmail.com>
2017-09-23 08:59:19 +0800

    add KSP

---
+++ codebook/graph/KSP.cpp
@@ -0,0 +1,146 @@
+// time: O(|E| \lg |E| + |V| \lg |V| + K)
+struct KSP{ // 1-base
+  struct nd{
+    int u, v, d;
+    nd(int ui = 0, int vi = 0, int di = INF){
+      u = ui; v = vi; d = di;
+    }
+  };
+  struct heap{
+    nd* edge; int dep; heap* chd[4];
+  };
+  static int cmp(heap* a,heap* b){
+    return a->edge->d > b->edge->d;
+  }
+  struct node{
+    int v; LL d; heap* H; nd* E;
+    node(){}
+    node(LL _d, int _v, nd* _E){
+      d =_d; v = _v; E = _E;
+    }
+    node(heap* _H, LL _d){
+      H = _H; d = _d;
+    }
+    friend bool operator<(node a, node b){
+      return a.d > b.d;
+    }
+  };
+  int n, k, s, t, dst[ N ];
+  nd *nxt[ N ];
+  vector<nd*> g[ N ], rg[ N ];
+  heap *nullNd, *head[ N ];
+  void init( int _n , int _k , int _s , int _t ){
+    n = _n; k = _k; s = _s; t = _t;
+    for( int i = 1 ; i <= n ; i ++ ){
+      g[ i ].clear();
+      rg[ i ].clear();
+      nxt[ i ] = NULL;
+      head[ i ] = NULL;
+      dst[ i ] = -1;
+    }
+  }
+  void addEdge( int ui , int vi , int di ){
+    nd* e = new nd(ui, vi, di);
+    g[ ui ].push_back( e );
+    rg[ vi ].push_back( e );
+  }
+  queue<int> dfsQ;
+  void dijkstra(){
+    while(dfsQ.size()) dfsQ.pop();
+    priority_queue<node> Q;
+    Q.push(node(0, t, NULL));
+    while (!Q.empty()){
+      node p = Q.top(); Q.pop();
+      if(dst[p.v] != -1) continue;
+      dst[ p.v ] = p.d;
+      nxt[ p.v ] = p.E;
+      dfsQ.push( p.v );
+      for(auto e: rg[ p.v ])
+        Q.push(node(p.d + e->d, e->u, e));
+    }
+  }
+  heap* merge(heap* curNd, heap* newNd){
+    if(curNd == nullNd) return newNd;
+    heap* root = new heap;
+    memcpy(root, curNd, sizeof(heap));
+    if(newNd->edge->d < curNd->edge->d){
+      root->edge = newNd->edge;
+      root->chd[2] = newNd->chd[2];
+      root->chd[3] = newNd->chd[3];
+      newNd->edge = curNd->edge;
+      newNd->chd[2] = curNd->chd[2];
+      newNd->chd[3] = curNd->chd[3];
+    }
+    if(root->chd[0]->dep < root->chd[1]->dep)
+      root->chd[0] = merge(root->chd[0],newNd);
+    else
+      root->chd[1] = merge(root->chd[1],newNd);
+    root->dep = max(root->chd[0]->dep, root->chd[1]->dep) + 1;
+    return root;
+  }
+  vector<heap*> V;
+  void build(){
+    nullNd = new heap;
+    nullNd->dep = 0;
+    nullNd->edge = new nd;
+    fill(nullNd->chd, nullNd->chd+4, nullNd);
+    while(not dfsQ.empty()){
+      int u = dfsQ.front(); dfsQ.pop();
+      if(!nxt[ u ]) head[ u ] = nullNd;
+      else head[ u ] = head[nxt[ u ]->v];
+      V.clear();
+      for( auto&& e : g[ u ] ){
+        int v = e->v;
+        if( dst[ v ] == -1 ) continue;
+        e->d += dst[ v ] - dst[ u ];
+        if( nxt[ u ] != e ){
+          heap* p = new heap;
+          fill(p->chd, p->chd+4, nullNd);
+          p->dep = 1;
+          p->edge = e;
+          V.push_back(p);
+        }
+      }
+      if(V.empty()) continue;
+      make_heap(V.begin(), V.end(), cmp);
+#define L(X) ((X<<1)+1)
+#define R(X) ((X<<1)+2)
+      for( size_t i = 0 ; i < V.size() ; i ++ ){
+        if(L(i) < V.size()) V[i]->chd[2] = V[L(i)];
+        else V[i]->chd[2]=nullNd;
+        if(R(i) < V.size()) V[i]->chd[3] = V[R(i)];
+        else V[i]->chd[3]=nullNd;
+      }
+      head[u] = merge(head[u], V.front());
+    }
+  }
+  vector<LL> ans;
+  void first_K(){
+    ans.clear();
+    priority_queue<node> Q;
+    if( dst[ s ] == -1 ) return;
+    ans.push_back( dst[ s ] );
+    if( head[s] != nullNd )
+      Q.push(node(head[s], dst[s]+head[s]->edge->d));
+    for( int _ = 1 ; _ < k and not Q.empty() ; _ ++ ){
+      node p = Q.top(), q; Q.pop();
+      ans.push_back( p.d );
+      if(head[ p.H->edge->v ] != nullNd){
+        q.H = head[ p.H->edge->v ];
+        q.d = p.d + q.H->edge->d;
+        Q.push(q);
+      }
+      for( int i = 0 ; i < 4 ; i ++ )
+        if( p.H->chd[ i ] != nullNd ){
+          q.H = p.H->chd[ i ];
+          q.d = p.d - p.H->edge->d + p.H->chd[ i ]->edge->d;
+          Q.push( q );
+        }
+    }
+  }
+  void solve(){
+    dijkstra();
+    build();
+    first_K();
+  }
+} solver;
