1c6de703e0ad587d787f914dada55a7831d5fed1
eddy1021 <c.c.hsu01@gmail.com>
2018-01-23 19:45:55 +0800

    AC NEERC 2015 i

--- contest/NEERC/Regional/2015/i.cpp
+++ contest/NEERC/Regional/2015/i.cpp
@@ -2,151 +2,123 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-struct Treap{
-	int sz , pri , gg , dg , mingg , id , res;
-  LL tv , totv;
-	Treap *l , *r;
-	Treap( int _val , int _id , int ngg , int _res ){
-    id = _id;
-    sz = 1;
-    res = _res;
-		pri = rand();
-    mingg = gg = dg = ngg;
-    totv = tv = _val;
-    l = r = NULL;
-	}
+struct Order{
+  int id , v , cv , tv;
 };
-void minus_g( Treap* a , int dlt ){
-  a->dg += dlt;
-  a->gg -= dlt;
-  a->mingg -= dlt;
+#define N 50505
+map<int,int> _id;
+set<int> wt4buy , wt4sell;
+int _mid;
+inline int ID( int price ){
+  auto it = _id.find( price );
+  if( it != _id.end() ) return it->second;
+  return _id[ price ] = _mid ++;
 }
-void push( Treap * a ){
-  if( a->dg ){
-    if( a->l ){
-      a->l->dg += a->dg;
-      a->l->gg -= a->dg;
-      a->l->mingg -= a->dg;
-    }else{
-      a->r->dg += a->dg;
-      a->r->gg -= a->dg;
-      a->r->mingg -= a->dg;
+deque<Order> sell[ N ] , buy[ N ];
+void print( const deque<Order>& dq , int T , int P ){
+  for( const auto& i : dq )
+    printf( "%d %d %d %d %d %d\n" ,
+            i.id , T , P , i.v , i.tv , i.cv );
+}
+map< tuple<int,int,int> , int > traded;
+void trade( int buy_id , int sell_id , int pri , int vv ){
+  tuple<int,int,int> tp = make_tuple( buy_id , sell_id , pri );
+  traded[ tp ] += vv;
+}
+bool decrease( Order& ord , int vv ){
+  ord.cv -= vv;
+  ord.v -= vv;
+  if( ord.cv <= 0 ){
+    ord.cv = min( ord.v , ord.tv );
+    return ord.cv > 0;
+  }
+  return false;
+}
+void trading( deque<Order>& wt_lst , int pri , int bid , int& V , int T ){
+  while( V > 0 and not wt_lst.empty() ){
+    int nsz = (int)wt_lst.size();
+    for( int _ = 0 ; _ < nsz and V > 0 ; _ ++ ){
+      int tk = min( V , wt_lst[ 0 ].cv );
+      if( T == 1 )
+        trade( bid , wt_lst[ 0 ].id , pri , tk );
+      else
+        trade( wt_lst[ 0 ].id , bid , pri , tk );
+      V -= tk;
+      if( decrease( wt_lst[ 0 ] , tk ) ){
+        wt_lst.push_back( wt_lst[ 0 ] );
+        wt_lst.pop_front();
+      }else if( wt_lst[ 0 ].cv == 0 )
+        wt_lst.pop_front();
     }
-    a->dg = 0;
-  }
-}
-int Size( Treap* a ){ return a ? a->sz : 0; }
-int Min( Treap* a ){ return a ? a->mingg : 1e9; }
-LL Tot( Treap* a ){ return a ? a->totv : 0; }
-void pull( Treap * a ){
-  a->mingg = min( a->gg , min( Min( a->l ) , Min( a->r ) ) );
-  a->totv = a->tv + Tot( a->l ) + Tot( a->r );
-	a->sz = Size( a->l ) + Size( a->r ) + 1;
-}
-Treap* merge( Treap *a , Treap *b ){
-	if( !a || !b ) return a ? a : b;
-	if( a->pri > b->pri ){
-		push( a );
-		a->r = merge( a->r , b );
-		pull( a );
-		return a;
-	}else{
-		push( b );
-		b->l = merge( a , b->l );
-		pull( b );
-		return b;
-	}
-}
-void split( Treap *t , int k , Treap*&a , Treap*&b ){
-	if( !t ){ a = b = NULL; return; }
-	push( t );
-	if( Size( t->l ) + 1 <= k ){
-		a = t;
-		split( t->r , k - Size( t->l ) - 1 , a->r , b );
-		pull( a );
-	}else{
-		b = t;
-		split( t->l , k , a , b->l );
-		pull( b );
-	}
-}
-map<int,Treap*> sell , buy;
-map< tuple<int,int,int> , int > trade;
-void gogogo( int cid , Treap* now , int pri , int nr ){
-  if( !now ) return;
-  trade[ make_tuple( cid , now->id , pri ) ] += nr * now->tv;
-}
-int find_til( Treap* now , int rv ){
-  int ret = 0;
-  push( now );
-  if( Tot( now->l ) <= rv and Min( now->l ) > 0 ){
-    if( Tot( now->l ) + now->tv <= rv and now->gg > 0 )
-      return SZ( now->l ) + 1 + find_til( now->r , rv - Tot( now->l ) - tv );
-    return SZ( now->l );
-  }
-  return find_til( now->l , rv );
-}
-void gogo( int cid , int& V , int pri , Treap*& root ){
-  int round = 0;
-  while( root and V > 0 ){
-    round += min( Min( root ) , V / Tot( root ) );
-
-    minus_g( root , round );
-
-    V -= round * Tot( root );
-    if( V ){
-      int til = find_til( root , V );
-      if( til ){
-        Treap *tl , *tm , *tr;
-        split(  )
-      }
+    if( V == 0 or wt_lst.empty() )
+      break;
+    int mx = 1e9;
+    LL totv = 0;
+    for( size_t i = 0 ; i < wt_lst.size() and mx > 0 ; i ++ ){
+      mx = min( mx , wt_lst[ i ].v / wt_lst[ i ].tv );
+      totv += wt_lst[ i ].tv;
+    }
+    mx = min( (LL)mx , V / totv );
+    if( mx == 0 ) continue;
+    nsz = (int)wt_lst.size();
+    while( nsz -- ){
+      if( T == 1 )
+        trade( bid , wt_lst[ 0 ].id , pri , mx * wt_lst[ 0 ].tv );
+      else
+        trade( wt_lst[ 0 ].id , bid , pri , mx * wt_lst[ 0 ].tv );
+      decrease( wt_lst[ 0 ] , mx * wt_lst[ 0 ].tv );
+      V -= mx * wt_lst[ 0 ].tv;
+      if( wt_lst[ 0 ].cv )
+        wt_lst.push_back( wt_lst[ 0 ] );
+      wt_lst.pop_front();
     }
   }
-  if( round )
-    gogogo( cid , root , pri , round );
-  return root;
+  if( wt_lst.empty() ){
+    if( T == 1 )
+      wt4buy.erase( pri );
+    else
+      wt4sell.erase( pri );
+  }
 }
-void Buy( int cid , int P , int V , int TV ){
-  while( not sell.empty() and sell.begin()->first <= P and V > 0 ){
-    gogo( cid , V , sell->begin()->first , sell.begin()->second );
-    if( sell.begin()->second == NULL )
-      sell.erase( sell.begin() );
+void Buy( int id , int P , int V , int TV ){
+  while( wt4buy.size() and *wt4buy.begin() <= P and V > 0 )
+    trading( sell[ ID( *wt4buy.begin() ) ] , *wt4buy.begin() , id , V , 1 );
+  if( V > 0 ){
+    wt4sell.insert( P );
+    int wid = ID( P );
+    buy[ wid ].push_back( { id , V , min( V , TV ) , TV } );
   }
-  if( V )
-    buy[ P ] = merge( buy[ P ] , new Treap( TV , cid , V / TV , V % TV ) );
 }
-void solve(){
-  int n; scanf( "%d" , &n ); while( n -- ){
-    int id , T , P , V , TV;
-    scanf( "%d%d%d%d%d" , &id , &T , &P , &V , &TV );
-    if( T == 1 ) Buy( id , P , V , TV );
-    else Sell( id , P , V , TV );
-  }
-  for( auto i : trade )
-    printf( "%d %d %d %d\n" , i.first.first , i.first.second ,
-                              i.second.first , i.second.second );
-  while( sell.size() and buy.size() ){
-    if( sell.begin()->first < buy.begin()->firs ){
-      print( sell.begin()->second , 2 );
-      sell.erase( sell.begin() );
-    }else{
-      print( buy.begin()->second , 1 );
-      buy.erase( buy.begin() );
-    }
-  }
-  while( sell.size() ){
-    print( sell.begin()->second , 2 );
-    sell.erase( sell.begin() );
-  }
-  while( buy.size() ){
-    print( buy.begin()->second , 1 );
-    buy.erase( buy.begin() );
+void Sell( int id , int P , int V , int TV ){
+  while( wt4sell.size() and *wt4sell.rbegin() >= P and V > 0 )
+    trading( buy[ ID( *wt4sell.rbegin() ) ] , *wt4sell.rbegin() , id , V , 2 );
+  if( V > 0 ){
+    wt4buy.insert( P );
+    int wid = ID( P );
+    sell[ wid ].push_back( { id , V , min( V , TV ) , TV } );
   }
 }
 int main(){
 #ifndef EDDY
-  freopen( ".in" , "r" , stdin );
-  freopen( ".out" , "w" , stdout );
+  freopen( "iceberg.in" , "r" , stdin );
+  freopen( "iceberg.out" , "w" , stdout );
 #endif
-  solve();
+  int _; scanf( "%d" , &_ ); while( _ -- ){
+    int id , T , P , V , TV;
+    scanf( "%d%d%d%d%d" , &id , &T , &P , &V , &TV );
+    if( T == 1 ) Buy( id , P , V , TV );
+    else Sell( id , P , V , TV );
+    for( auto i : traded )
+      printf( "%d %d %d %d\n" ,
+              get<0>( i.first ) ,
+              get<1>( i.first ) ,
+              get<2>( i.first ) ,
+              i.second );
+    traded.clear();
+  }
+  puts( "" );
+  for( auto i : _id ){
+    print( buy[ i.second ] , 1 , i.first );
+    print( sell[ i.second ] , 2 , i.first );
+  }
 }
