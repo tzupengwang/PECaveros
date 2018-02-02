1584c0b52955f704f232b9b3963f3ec0a85d8cc5
tzupengwang <tzupengwang@gmail.com>
2018-02-03 00:35:54 +0800

    AC XV Udmurtia F

--- contest/opencup/XV/Udmurtia/f2.cpp
+++ contest/opencup/XV/Udmurtia/f2.cpp
@@ -2,7 +2,7 @@
 #pragma GCC optimize("O3")
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
+typedef long long ll;
 
 int n, Q;
 int t[100005], c[100005];
@@ -10,6 +10,8 @@ vector<int> gp[105];
 
 bool tagged[100005];
 int psum[105][100005];
+ll cnt[105];
+int cnt2[105];
 
 int main(){
   scanf("%d", &n);
@@ -18,6 +20,8 @@ int main(){
     gp[t[i]].push_back(i);
 
     psum[t[i]][c[i]] ++;
+    cnt[t[i]] += c[i];
+    cnt2[t[i]] ++;
   }
   for (int i = 1; i <= 100; i ++) {
     for (int j = 1; j <= 100000; j ++)
@@ -36,8 +40,21 @@ int main(){
 
       tagged[q - t[x]] = true;
       psum[t[x]][-- c[x]] ++;
+      cnt[t[x]] --;
 
-      if (c[x] == 0) ans1 --;
+      if (c[x] == 0) ans1 --, cnt2[t[x]] --;
+    }
+    ll tsum = 0;
+    int tsum2 = 0, ps = 0, bad = 0;
+
+    for (int i = 99; i >= 1; i --) {
+      if (q >= i && !tagged[q - i]) {
+        ps ++;
+      }
+      tsum += cnt[i + 1];
+      tsum2 += cnt2[i + 1];
+      if (tsum <= ps) bad = tsum2;
+      assert(tsum >= ps);
     }
 
     int tg = 0;
@@ -52,7 +69,7 @@ int main(){
     }
     ans2 += n;
 
-    printf("%d %d\n", ans1, ans2);
+    printf("%d %d\n", ans1 - bad, ans2);
   }
 
 }
