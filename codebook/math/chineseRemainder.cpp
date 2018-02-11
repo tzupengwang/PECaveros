636d258a3031ed0c1bc992dc1aa3275c70800ae2
eddy1021 <c.c.hsu01@gmail.com>
2018-02-11 22:35:11 +0800

    fix chinese remainder

--- codebook/math/chineseRemainder.cpp
+++ codebook/math/chineseRemainder.cpp
@@ -1,7 +1,9 @@
 LL solve(LL x1, LL m1, LL x2, LL m2) {
+  LL g = __gcd(m1, m2);
+  if((x2 - x1) % g) return -1;// no sol
+  m1 /= g; m2 /= g;
   pair<LL,LL> p = gcd(m1, m2);
-  // m1 * p.first + m2 * p.second = 1
-  LL lcm = m1 * m2;
+  LL lcm = m1 * m2 * g;
   LL res = p.first * (x2 - x1) * m1 + x1;
   return (res % lcm + lcm) % lcm;
 }
