9aaa878a512767cdf7553f45a4ecbd8a89c5c419
eddy1021 <c.c.hsu01@gmail.com>
2018-02-24 17:02:22 +0800

    ac WF 2012 a

--- contest/WorldFinals/2012/a.cpp
+++ contest/WorldFinals/2012/a.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-#define double long double
+//#define double long double
 const double eps = 5e-7;
 const double eps2 = 1e-9;
 const double inf = 1e100;
@@ -16,6 +16,9 @@ pt operator+( const pt& p1 , const pt& p2 ){
 pt operator-( const pt& p1 , const pt& p2 ){
   return {p1.X - p2.X, p1.Y - p2.Y};
 }
+double operator*( const pt& p1 , const pt& p2 ){
+  return p1.X * p2.X + p1.Y * p2.Y;
+}
 double operator^( const pt& p1 , const pt& p2 ){
   return p1.X * p2.Y - p1.Y * p2.X;
 }
@@ -25,9 +28,11 @@ pt operator*( const pt& pp , double tk ){
 bool in( double lx , double xx , double rx ){
   return lx - eps2 < xx and xx < rx + eps2;
 }
+bool in( const pt& p1 , const pt& cp , const pt& p2 ){
+  return (cp - p1) * (p2 - p1) > -eps2 and
+         (cp - p2) * (p1 - p2) > -eps2; 
+}
 pt interPnt( pt p1, pt p2, pt q1, pt q2, bool& res){
-  if( p1 > p2 ) swap( p1 , p2 );
-  if( q1 > q2 ) swap( q1 , q2 );
 	double f1 = ( p2 - p1 ) ^ ( q1 - p1 );
 	double f2 = ( p2 - p1 ) ^ ( p1 - q2 );
 	double f = ( f1 + f2 );
@@ -37,9 +42,9 @@ pt interPnt( pt p1, pt p2, pt q1, pt q2, bool& res){
   }
   res = true;
 	pt ret_p = q1 * ( f2 / f ) + q2 * ( f1 / f );
-  if( not in( p1.X , ret_p.X , p2.X ) )
+  if( not in( p1 , ret_p , p2 ) )
     res = false;
-  if( not in( q1.X , ret_p.X , q2.X ) )
+  if( not in( q1 , ret_p , q2 ) )
     res = false;
   return ret_p;
 }
@@ -64,13 +69,26 @@ struct Pt{
 double norm( const Pt& pp ){
   return sqrt( pp * pp );
 }
+double norm2( const Pt& pp ){
+  return pp * pp;
+}
 #define N 55
+#define K 60
 int n;
 Pt p[ N ] , v[ N ] , np[ N ];
 bool gt[ N ];
 bitset<N> tk[ N ] , cur[ N ];
 double bst[ N ];
 int fr[ N ];
+double dist( int id1 , int id2 , double _t ){
+  return norm( (p[ id1 ] + v[ id1 ] * _t) -
+               (p[ id2 ] + v[ id2 ] * _t) );
+}
+double dist2( int id1 , int id2 , double _t ){
+  return norm2( (p[ id1 ] + v[ id1 ] * _t) -
+                (p[ id2 ] + v[ id2 ] * _t) );
+}
+int recal = 0;
 void cal_mst( double _t ){
   for( int i = 0 ; i < n ; i ++ ){
     np[ i ] = p[ i ] + v[ i ] * _t;
@@ -93,7 +111,8 @@ void cal_mst( double _t ){
     gt[ who ] = true;
     for( int j = 0 ; j < n ; j ++ ){
       if( gt[ j ] ) continue;
-      double td = norm( np[ who ] - np[ j ] );
+      //double td = norm( np[ who ] - np[ j ] );
+      double td = dist( who , j , _t );
       if( td < bst[ j ] - eps2 ){
         bst[ j ] = td;
         fr[ j ] = who;
@@ -101,32 +120,22 @@ void cal_mst( double _t ){
     }
   }
 }
-double dist( int id1 , int id2 , double _t ){
-  return norm( (p[ id1 ] + v[ id1 ] * _t) -
-               (p[ id2 ] + v[ id2 ] * _t) );
-}
 int _cs;
-vector< pair<pt,pt> > f( int id1 , int id2 ){
-  vector< pair<pt,pt> > ret;
-  if( dist( id1 , id2 , 0 ) < dist( id1 , id2 , eps ) ){
-    ret.push_back( { { 0       , dist( id1 , id2 , 0       ) } ,
-                     { forever , dist( id1 , id2 , forever ) } } );
-  }else{
-    double bl = 0 , br = forever;
-    for( int i = 0 ; i < N ; i ++ ){
-      double mid1 = (bl + bl + br) / 3;
-      double mid2 = (bl + br + br) / 3;
-      if( dist( id1 , id2 , mid1 ) < dist( id1 , id2 , mid2 ) )
-        br = mid2;
-      else
-        bl = mid1;
-    }
-    ret.push_back( { { 0       , dist( id1 , id2 , 0 ) } ,
-                     { bl      , dist( id1 , id2 , bl ) } } );
-    ret.push_back( { { bl      , dist( id1 , id2 , bl ) } ,
-                     { forever , dist( id1 , id2 , forever ) } } );
+vector<double> calc( const Pt& p1 , const Pt& v1 , 
+                     const Pt& p2 , const Pt& v2 ){
+  // ax^2+bx+c = 0
+  double a = v1 * v1 - v2 * v2;
+  double b = 2 * (p1 * v1 - p2 * v2);
+  double c = p1 * p1 - p2 * p2;
+  if( fabs( a ) < eps2 ){
+    if( fabs( b ) < eps2 ) return {};
+    return {-c / b};
   }
-  return ret;
+  double bb4ac = b * b - 4. * a * c;
+  if( bb4ac < -eps2 ) return {};
+  bb4ac = max( bb4ac , 0. );
+  return { (-b + sqrt(bb4ac)) / (2. * a) ,
+           (-b - sqrt(bb4ac)) / (2. * a) };
 }
 void solve(){
   vector< pair<double,pair< pair<int,int> , pair<int,int> > > > eve;
@@ -138,16 +147,12 @@ void solve(){
     for( size_t j = i + 1 ; j < vv.size() ; j ++ ){
       int id1 = vv[ i ].first , id2 = vv[ i ].second;
       int jd1 = vv[ j ].first , jd2 = vv[ j ].second;
-      vector< pair<pt,pt> > func1 = f( id1 , id2 );
-      vector< pair<pt,pt> > func2 = f( jd1 , jd2 );
-      for( auto ii : func1 )
-        for( auto jj : func2 ){
-          bool rr;
-          pt ret = interPnt( ii.first , ii.second ,
-                             jj.first , jj.second , rr );
-          if( !rr ) continue;
-          eve.push_back( { ret.X , { {id1, id2}, {jd1, jd2} } } );
-        }
+      vector<double> cand = calc( p[ id2 ] - p[ id1 ] , v[ id2 ] - v[ id1 ] ,
+                                  p[ jd2 ] - p[ jd1 ] , v[ jd2 ] - v[ jd1 ] );
+      for( auto tt : cand ){
+        if( tt < eps ) continue;
+        eve.push_back( { tt , { { id1 , id2 } , { jd1 , jd2 } } } );
+      }
     }
   sort( eve.begin() , eve.end() );
   cal_mst( 0 );
@@ -155,8 +160,8 @@ void solve(){
   for( int i = 0 ; i < n ; i ++ )
     cur[ i ] = tk[ i ];
   for( auto i : eve ){
-    if( not cur[ i.second.first.first  ][ i.second.first.second  ] and
-        not cur[ i.second.second.first ][ i.second.second.second ] )
+    if( !cur[ i.second.first.first  ][ i.second.first.second  ] and
+        !cur[ i.second.second.first ][ i.second.second.second ] )
       continue;
     cal_mst( i.first + eps );
     bool chg = false;
