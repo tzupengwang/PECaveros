63110ca6441c1b1f89084009fed312df8009e580
eddy1021 <c.c.hsu01@gmail.com>
2018-03-12 12:00:53 +0800

    AC WF pK, spiritual AC WF pE

--- contest/WorldFinals/2010/e.cpp
+++ contest/WorldFinals/2010/e.cpp
@@ -3,181 +3,220 @@
 using namespace std;
 typedef long long LL;
 #define N 22
+struct DJS{
+  vector<int> v;
+  vector< vector<int> > history;
+  void init( int n ){
+    v.resize( n );
+    for( int i = 0 ; i < n ; i ++ )
+      v[ i ] = i;
+    history.clear();
+  }
+  int f( int x ){
+    return x == v[ x ] ? x : f( v[ x ] );
+  }
+  void uni( int x , int y ){
+    v[ f( x ) ] = f( y );
+  }
+  void save(){
+    history.push_back( v );
+  }
+  void undo(){
+    v = history.back();
+    history.pop_back();
+  }
+} djs;
 int r , c;
-LL encode( int* a ){
+LL encode( int* arr ){
   LL ret = 0;
   for( int i = 0 ; i < c ; i ++ )
-    ret = (ret << 5) | a[ i ];
+    ret = (ret << 5) | arr[ i ];
   return ret;
 }
-void decode( LL x , int* a ){
+void decode( LL enc , int* arr ){
   for( int i = c - 1 ; i >= 0 ; i -- ){
-    a[ i ] = x & ((1 << 5) - 1);
-    x >>= 5;
+    arr[ i ] = enc & ((1LL << 5) - 1);
+    enc >>= 5;
   }
 }
-int score( int* a ){
+int score( int* arr ){
   int ret = 0;
   for( int i = 0 ; i < c ; i ++ )
-    if( a[ i ] )
+    if( arr[ i ] )
       ret ++;
   return ret;
 }
+unordered_map<LL,LL> bk[ N ];
+unordered_map<LL,int> dp[ N ];
 char a[ N ][ N ];
-void init(){
-  for( int i = 1 ; i <= r ; i ++ )
-    scanf( "%s" , a[ i ] );
-}
-unordered_map< LL , int > st[ N ];
-unordered_map< LL , LL > bk[ N ];
 bool block[ N ];
-int cur_row , to , ori[ 10 ] , now[ 10 ];
-int cand[ 10 ] , ct , pscore;
+int ori[ N ] , now[ N ] , to , pscore;
 LL from;
 void upd(){
-  LL ev = encode( now );
-  int nscore = pscore;
-  for( int i = 0 ; i < c ; i ++ )
-    if( now[ i ] )
-      nscore ++;
-  auto it = st[ cur_row ].find( ev );
-  if( it == st[ cur_row ].end() or
+  int rnow[ N ] = {} , iid = 0;
+  int map_to[ N ] = {};
+  for( int i = 0 ; i < c ; i ++ ){
+    if( !now[ i ] ) continue;
+    int rp = djs.f( now[ i ] >> 1 );
+    if( !map_to[ rp ] )
+      map_to[ rp ] = ++ iid;
+    rnow[ i ] = (map_to[ rp ] << 1) | (now[ i ] & 1);
+  }
+  LL nstate = encode( rnow );
+  int nscore = pscore + score( rnow );
+  auto it = dp[ to ].find( nstate );
+  if( it == dp[ to ].end() or
       nscore > it->second ){
-    st[ cur_row ][ ev ] = nscore;
-    bk[ cur_row ][ ev ] = from;
+    dp[ to ][ nstate ] = nscore;
+    bk[ to ][ nstate ] = from;
   }
 }
-int map_to[ 111 ];
-void gogo( int no , int pre , int iid , int lst ){ // >= pre
-  if( no == ct ){
+void go( int cur_c , int pre , int nid ){
+  if( cur_c >= c ){
     upd();
     return;
   }
+  if( ori[ cur_c ] & 1 ){
+    if( pre > cur_c ) return;
+    if( block[ cur_c ] ) return;
+    int val = (ori[ cur_c ] >> 1);
+    int nxt = cur_c + 1;
+    while( nxt < c and !(ori[ nxt ] & 1) ) nxt ++;
+    { // union with next
+      if( nxt < c and (ori[ cur_c ] >> 1) != (ori[ nxt ] >> 1) ){
+        bool ok = true;
+        for( int j = cur_c ; j <= nxt and ok ; j ++ )
+          if( block[ j ] )
+            ok = false;
+        for( int j = cur_c + 1 ; j < nxt and ok ; j ++ )
+          if( ori[ j ] )
+            ok = false;
+        if( ok ){
+          for( int j = cur_c ; j <= nxt ; j ++ )
+            now[ j ] = (val << 1);
+          djs.save();
+          djs.uni( ori[ cur_c ] >> 1 , ori[ nxt ] >> 1 );
 
-  {
-    gogo( no , pre + 1 , iid , lst );
-    for( int i = pre ; i <= cand[ no ] - 2 ; i ++ ){
+          go( nxt + 2 , nxt + 2 , nid );
+
+          djs.undo();
+          for( int j = cur_c ; j <= nxt ; j ++ )
+            now[ j ] = 0;
+        }
+      }
+    }
+    { // directly down
+      now[ cur_c ] = (val << 1) | 1;
+      go( cur_c + 2 , cur_c + 2 , nid );
+      now[ cur_c ] = 0;
+    }
+    now[ cur_c ] = (val << 1);
+    { // extend to pre
+      for( int i = cur_c - 1 ; i >= pre ; i -- ){
+        if( block[ i ] or ori[ i ] ) break;
+        now[ i ] = (val << 1) | 1;
+        if( i == 0 or !ori[ i - 1 ] )
+          go( cur_c + 2 , cur_c + 2 , nid );
+        now[ i ] = (val << 1);
+      }
+      for( int i = cur_c - 1 ; i >= pre ; i -- ){
+        if( block[ i ] or ori[ i ] ) break;
+        now[ i ] = 0;
+      }
+    }
+    { // extend to nxt
+      int til = (nxt == c ? c - 1 : nxt - 2);
+      for( int i = cur_c + 1 ; i <= til ; i ++ ){
+        if( block[ i ] or ori[ i ] ) break;
+        now[ i ] = (val << 1) | 1;
+        if( i == c - 1 or !ori[ i + 1 ] )
+          go( i + 2 , i + 2 , nid );
+        now[ i ] = (val << 1);
+      }
+      for( int i = cur_c + 1 ; i <= til ; i ++ ){
+        if( block[ i ] or ori[ i ] ) break;
+        now[ i ] = 0;
+      }
+    }
+    now[ cur_c ] = 0;
+  }else{
+    go( cur_c + 1 , pre , nid );
+    if( cur_c and ori[ cur_c - 1 ] ) return;
+    if( cur_c < pre ) return;
+    int nxt = cur_c + 1;
+    while( nxt < c and !(ori[ nxt ] & 1) ) nxt ++;
+    int til = (nxt == c ? c - 1 : nxt - 2);
+    // new nid
+    for( int i = cur_c ; i <= til ; i ++ ){
       if( block[ i ] or ori[ i ] ) break;
-      now[ i ] = (iid + 1) << 1;
-      
+      now[ i     ] = (nid << 1) | 1;
+      now[ cur_c ] = (nid << 1) | 1;
+      if( i - cur_c + 1 > 2 and (i == c - 1 or !ori[ i + 1 ]) )
+        go( i + 2 , i + 2 , nid + 1 );
+      now[ i     ] = (nid << 1);
+    }
+    for( int i = cur_c ; i <= til ; i ++ ){
+      if( block[ i ] or ori[ i ] ) break;
       now[ i ] = 0;
     }
   }
-
-  int Ori = map_to[ ori[ cand[ no ] ] >> 1 ];
-  if( !map_to[ ori[ cand[ no ] ] >> 1 ] )
-    map_to[ ori[ cand[ no ] ] >> 1 ] = ++ iid;
-  int val = map_to[ ori[ cand[ no ] ] >> 1 ]; 
-  if( no + 1 < ct and (ori[ cand[ no     ] ] >> 1) !=
-                       (ori[ cand[ no + 1 ] ] >> 1) ){
-    bool ok = true;
-    for( int k = cand[ no ] ; k <= cand[ no + 1 ] ; k ++ ){
-      if( block[ k ] ){
-        ok = false;
-        break;
-      }
-      if( k > cand[ no ] and k < cand[ no + 1 ] and ori[ k ] ){
-        ok = false;
-        break;
-      }
-    }
-    if( ok ){
-      for( int k = cand[ no ] ; k <= cand[ no + 1 ] ; k ++ )
-        now[ k ] = val << 1;
-      gogo( no + 2 , cand[ no + 1 ] + 2 , iid , lst );
-      for( int k = cand[ no ] ; k <= cand[ no + 1 ] ; k ++ )
-        now[ k ] = 0;
-    }
-  }
-  if( not block[ cand[ no ] ] ){
-    if( cand[ no ] + 1 == c or !ori[ cand[ no ] + 1 ] ){
-      for( int i = cand[ no ] ; i >= pre ; i -- ){
-        if( block[ i ] ) break;
-        if( i < cand[ no ] and ori[ i ] ) break;
-        now[ i ] = (val << 1) | 1;
-        gogo( no + 1 , cand[ no ] + 2 , iid , lst );
-        now[ i ] = (val << 1);
-      }
-      for( int i = cand[ no ] ; i >= pre ; i -- ){
-        if( block[ i ] ) break;
-        now[ i ] = 0;
-      }
-    }
-    if( cand[ no ] == 0 or !ori[ cand[ no ] - 1 ] ){
-      int til = (no + 1 == ct ? c - 1 : cand[ no + 1 ] - 2);
-      for( int i = cand[ no ] ; i <= til ; i ++ ){
-        if( block[ i ] ) break;
-        if( i > cand[ no ] and ori[ i ] ) break;
-        now[ i ] = (val << 1) | 1;
-        gogo( no + 1 , i + 2 , iid , lst );
-        now[ i ] = (val << 1);
-      }
-      for( int i = cand[ no ] ; i <= til ; i ++ ){
-        if( block[ i ] ) break;
-        now[ i ] = 0;
-      }
-    }
-  }
-  map_to[ ori[ cand[ no ] ] >> 1 ] = Ori;
 }
 int ans[ N ][ N ] , _cs;
 void solve(){
-  for( int i = 0 ; i <= r ; i ++ ){
-    st[ i ].clear();
+  for( int i = 0 ; i < N ; i ++ ){
     bk[ i ].clear();
+    dp[ i ].clear();
   }
   {
-    int start[ 10 ];
-    for( int i = 0 ; i < c ; i ++ )
-      start[ i ] = 0;
-    start[ 0 ] = (1 << 1) | 1;
-    st[ 0 ][ encode( start ) ] = 0;
+    int ini[ N ] = {};
+    ini[ 0 ] = (1LL << 1) | 1;
+    dp[ 0 ][ encode( ini ) ] = 0;
   }
-  for( int i = 0 ; i + 1 <= r ; i ++ ){
-    cur_row = i + 1;
-    for( int j = 0 ; j < c ; j ++ )
-      if( a[ i + 1 ][ j ] == '#' )
-        block[ j ] = true;
-      else
-        block[ j ] = false;
+  for( int i = 0 ; i < r ; i ++ ){
     to = i + 1;
-    for( auto cur : st[ i ] ){
-      from = cur.first;
-      decode( cur.first , ori );
-      pscore = cur.second;
-      ct = 0;
+    for( int j = 0 ; j < c ; j ++ )
+      block[ j ] = (a[ i + 1 ][ j ] == '#');
+    for( auto _st : dp[ i ] ){
+      from = _st.first;
+      pscore = _st.second;
+      djs.init( N );
+      decode( from , ori );
+      int mx = 0;
       for( int j = 0 ; j < c ; j ++ )
         if( ori[ j ] & 1 )
-          cand[ ct ++ ] = j;
-      for( int j = 0 ; j < c ; j ++ )
-        now[ j ] = 0;
-      gogo( 0 , 0 , 0 , 100 );
+          mx = max( mx , ori[ j ] >> 1 );
+      go( 0 , 0 , mx + 1 );
     }
   }
   int bst = -1; LL who = 0;
-  for( auto cur : st[ r ] ){
-    int arr[ 10 ];
-    decode( cur.first , arr );
-    int msk = 0;
-    for( int i = 0 ; i < c ; i ++ ){
-      if( !arr[ i ] ) continue;
-      msk |= (1 << (arr[ i ] >> 1));
-    }
+  for( auto _st : dp[ r ] ){
+    from = _st.first;
+    pscore = _st.second;
+    int lst[ N ];
+    decode( from , lst );
+    int msk = 0 , cnt = 0;
+    for( int i = 0 ; i < c ; i ++ )
+      if( lst[ i ] ){
+        msk |= (1 << (lst[ i ] >> 1));
+        cnt += lst[ i ] & 1;
+      }
     if( __builtin_popcount( msk ) != 1 )
       continue;
-    if( (arr[ c - 1 ] & 1) != 1 )
+    if( cnt > 1 )
       continue;
-    if( cur.second > bst ){
-      bst = cur.second;
-      who = cur.first;
+    if( !(lst[ c - 1 ] & 1) )
+      continue;
+    if( pscore > bst ){
+      bst = pscore;
+      who = from;
     }
   }
-  cerr << bst << endl;
-  printf( "Case %d:\n" , ++ _cs );
+  //cerr << bst << endl;
   for( int i = r ; i >= 1 ; i -- ){
     decode( who , ans[ i ] );
     who = bk[ i ][ who ];
   }
+  printf( "Case %d:\n" , ++ _cs );
   for( int i = 1 ; i <= r ; i ++ , puts( "" ) )
     for( int j = 0 ; j < c ; j ++ )
       if( a[ i ][ j ] == '#' )
@@ -186,10 +225,12 @@ void solve(){
         putchar( 'C' );
       else
         putchar( '.' );
+  puts( "" );
 }
 int main(){
-  while( scanf( "%d%d" , &r , &c ) == 2 and r ){
-    init();
+  while( cin >> r >> c and r ){
+    for( int i = 1 ; i <= r ; i ++ )
+      cin >> a[ i ];
     solve();
   }
 }
--- contest/WorldFinals/2010/k.cpp
+++ contest/WorldFinals/2010/k.cpp
@@ -2,16 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-const double eps = 1e-8;
+#define double long double
 struct Pt{
-  double X , Y , Z;
+  LL X , Y , Z;
   bool read(){
-    if( scanf( "%lf%lf%lf" , &X , &Y , &Z ) == 3 )
+    if(cin >> X >> Y >> Z)
       return true;
     return false;
   }
   void print(){
-    fprintf( stderr , "%.2f %.2f %.2f\n" , X , Y , Z );
+    //fprintf( stderr , "%.2f %.2f %.2f\n" , X , Y , Z );
+  }
+  bool operator<( const Pt& pp ) const{
+    return tie(X ,Y, Z ) < tie(pp.X, pp.Y, pp.Z);
   }
 };
 Pt operator+( const Pt& p1 , const Pt& p2 ){
@@ -20,17 +23,12 @@ Pt operator+( const Pt& p1 , const Pt& p2 ){
 Pt operator-( const Pt& p1 , const Pt& p2 ){
   return {p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z};
 }
-Pt operator*( const Pt& pp , double tk ){
+Pt operator*( const Pt& pp , LL tk ){
   return {pp.X * tk ,
           pp.Y * tk ,
           pp.Z * tk };
 }
-Pt operator/( const Pt& pp , double tk ){
-  return {pp.X / tk ,
-          pp.Y / tk ,
-          pp.Z / tk };
-}
-double operator*( const Pt& p1 , const Pt& p2 ){
+LL operator*( const Pt& p1 , const Pt& p2 ){
   return p1.X * p2.X + p1.Y * p2.Y + p1.Z * p2.Z;
 }
 Pt operator^( const Pt& p1 , const Pt& p2 ){
@@ -38,67 +36,106 @@ Pt operator^( const Pt& p1 , const Pt& p2 ){
           p1.Z * p2.X - p1.X * p2.Z,
           p1.X * p2.Y - p1.Y * p2.X};
 }
-double norm( const Pt& pp ){
-  return sqrt( pp * pp );
-}
-double norm2( const Pt& pp ){
+LL norm2( const Pt& pp ){
   return pp * pp;
 }
-Pt a , b , c , d , e , f;
-Pt project( const Pt& p1 , const Pt& p2 ){
-  return p2 * ((p1 * p2) / norm2( p2 ));
+double norm( const Pt& pp ){
+  return sqrtl( pp * pp );
 }
-Pt find_center(){
-  Pt cenab = (a + b) * .5;
-  Pt cen = (cenab * 2. + c ) / 3;
-  Pt cend = (cen * 3. + d) / 4;
-  Pt cene = (cen * 3. + e) / 4;
-  Pt far = (b - a) ^ (c - a);
-  double vold = norm( project( d - a , far ) );
-  double vole = norm( project( e - a , far ) );
-  return (cend * vold + cene * vole) / (vold + vole);
+Pt a , b , c , d , e , f;
+double cenx, ceny, cenz;
+LL volume( const Pt& p1 , const Pt& p2 , const Pt& p3 ){
+  return abs( (p1 ^ p2) * p3 );
+}
+void find_center(){
+  Pt cend = (a + b + c + d); // /4
+  Pt cene = (a + b + c + e); // /4
+  LL vold = volume( b - a , c - a , d - a );
+  LL vole = volume( b - a , c - a , e - a );
+  Pt cen = cend * vold + cene * vole;
+  LL dcen = 4 * (vold + vole);
+  cenx = cen.X / (double)dcen;
+  ceny = cen.Y / (double)dcen;
+  cenz = cen.Z / (double)dcen;
+  cerr << cenx << " " << ceny << " " << cenz << endl;
 }
 double min_ans , max_ans;
 bool got;
 
-void test( const Pt& fa , const Pt& pp , bool& lft , bool& rgt ){
-  double v = fa * pp;
-  if( v < -eps ) lft = true;
-  if( v > +eps ) rgt = true;
+Pt p0;
+bool out( const Pt& p1 , const Pt& p2 , const Pt& far ){
+  Pt ver = far ^ (p2 - p1);
+  double dst = 0.;
+  dst += (cenx - p0.X - p1.X) * ver.X;
+  dst += (ceny - p0.Y - p1.Y) * ver.Y;
+  dst += (cenz - p0.Z - p1.Z) * ver.Z;
+  double len = 0.;
+  len += (double)ver.X * ver.X;
+  len += (double)ver.Y * ver.Y;
+  len += (double)ver.Z * ver.Z;
+  dst /= sqrtl( len );
+  return dst < 0.2;
 }
 
-void cal( Pt p1 , Pt p2 , Pt p3 , Pt cen , Pt goal , Pt o1 , Pt o2 ){
-
-  bool left = false , right = false;
-
+void cal( Pt p1 , Pt p2 , Pt p3 , Pt goal , Pt o1 , Pt o2 ){
+  p0 = p1;
   p2 = p2 - p1;
   p3 = p3 - p1;
-  cen = cen - p1;
   goal = goal - p1;
   o1 = o1 - p1;
   o2 = o2 - p1;
-  
+  p1 = {0, 0};
   Pt far = p2 ^ p3;
 
-  test( far , o1 , left , right );
-  test( far , o2 , left , right );
-  if( left and right ) return;
-
-  Pt x = cen - project( cen , far );
-  Pt x2 = x - project( x , p2 );
-  Pt x3 = x - project( x , p3 );
-  Pt x4 = x - p2 - project( x - p2 , p3 - p2 );
-
-  double t1 = (x * p2) / norm2( p2 );
-  double t2 = (x * p3) / norm2( p3 );
-  double cur = norm( project( goal , far ) );
-  if( t1 < -eps or t2 < -eps or t1 + t2 > 1 + eps ){
-    if( norm( x2 ) > 0.2 and
-        norm( x3 ) > 0.2 and
-        norm( x4 ) > 0.2 )
+  double cur = (goal * far) / norm( far );
+  
+  if( norm( far ) == 0 ) return;
+  for( Pt pp : {goal, o1, o2} )
+    if( pp * far < 0 )
       return;
+
+  Pt other;
+  bool got_other = false;
+  for( Pt pp : {o1, o2} )
+    if( far * pp == 0 ){
+      other = pp;
+      got_other = true;
+    }
+  //cerr << cur << endl;
+  if( not got_other ){
+    if( out( p1 , p2 , far ) ) return;
+    if( out( p2 , p3 , far ) ) return;
+    if( out( p3 , p1 , far ) ) return;
+  }else{
+    bool in1 = ((other - p1) ^ (other - p2)) *
+               ((other - p1) ^ (other - p3)) <= 0;
+    bool in2 = ((other - p2) ^ (other - p1)) *
+               ((other - p2) ^ (other - p3)) <= 0;
+    bool in3 = ((other - p3) ^ (other - p1)) *
+               ((other - p3) ^ (other - p2)) <= 0;
+    if( in1 and in2 and in3 ){
+      if( out( p1 , p2 , far ) ) return;
+      if( out( p2 , p3 , far ) ) return;
+      if( out( p3 , p1 , far ) ) return;
+    }else if( in1 ){
+      if( out( p1 , p2 , far ) ) return;
+      if( out( p2 , other , far ) ) return;
+      if( out( other , p3 , far ) ) return;
+      if( out( p3 , p1 , far ) ) return;
+    }else if( in2 ){
+      if( out( p2 , p3 , far ) ) return;
+      if( out( p3 , other , far ) ) return;
+      if( out( other , p1 , far ) ) return;
+      if( out( p1 , p2 , far ) ) return;
+    }else if( in3 ){
+      if( out( p3 , p1 , far ) ) return;
+      if( out( p1 , other , far ) ) return;
+      if( out( other , p2 , far ) ) return;
+      if( out( p2 , p3 , far ) ) return;
+    }
   }
-  cerr << cur << endl;
+
+
   if( not got ){
     min_ans = max_ans = cur;
     got = true;
@@ -110,18 +147,15 @@ void cal( Pt p1 , Pt p2 , Pt p3 , Pt cen , Pt goal , Pt o1 , Pt o2 ){
 int _cs;
 void solve(){
   got = false;
-  Pt cen = find_center();
-  cen.print();
-  cal( a , b , d , cen , f , c , e );
-  cal( a , b , e , cen , f , c , d );
-  cal( a , c , d , cen , f , b , e );
-  cal( a , c , e , cen , f , b , d );
-  cal( b , c , d , cen , f , a , e );
-  cal( b , c , e , cen , f , a , d );
-  cal( d , e , a , cen , f , b , c );
-  cal( d , e , b , cen , f , a , c );
-  cal( d , e , c , cen , f , a , b );
-  printf( "Case %d: %.5f %.5f\n" , ++ _cs , min_ans , max_ans );
+  find_center();
+  Pt p[ 5 ] = {a, b, c, d, e};
+  int id[ 5 ] = {0, 1, 2, 3, 4};
+  do cal( p[ id[ 0 ] ] , p[ id[ 1 ] ] , p[ id[ 2 ] ] , f , p[ id[ 3 ] ] , p[ id[ 4 ] ] );
+  while( next_permutation( id , id + 5 ) );
+  printf( "Case %d: " , ++ _cs );
+  cout << fixed << setprecision( 5 );
+  cout << min_ans << " ";
+  cout << max_ans << "\n";
 }
 int main(){
   while( a.read() ){
