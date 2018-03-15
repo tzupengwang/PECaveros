02a59c5a87a08845fe94dae321ecfafbccc48284
eddy1021 <c.c.hsu01@gmail.com>
2018-03-15 21:30:54 +0800

    add python decimal note

---
+++ codebook/basic/python_trick.py
@@ -0,0 +1,14 @@
+from decimal import Decimal, getcontext
+getcontext().prec = 250 # set precision
+
+itwo = Decimal(0.5)
+two = Decimal(2)
+
+N = 200
+def angle(cosT):
+  """given cos(theta) in decimal return theta"""
+  for i in range(N):
+    cosT = ((cosT + 1) / two) ** itwo
+  sinT = (1 - cosT * cosT) ** itwo
+  return sinT * (2 ** N)
+pi = angle(Decimal(-1))
